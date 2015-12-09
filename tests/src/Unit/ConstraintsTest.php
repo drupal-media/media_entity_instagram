3cab88fdbaaf3f66b706866007a1c887e2f243a1
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-09 08:05:26 -0500

    Fixed broken tests.

--- tests/src/Unit/ConstraintsTest.php
+++ tests/src/Unit/ConstraintsTest.php
@@ -7,9 +7,8 @@
 
 namespace Drupal\Tests\media_entity_instagram\Unit;
 
-use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\Plugin\Field\FieldType\StringLongItem;
+use Drupal\Core\TypedData\ComplexDataDefinitionInterface;
 use Drupal\media_entity_instagram\Plugin\Validation\Constraint\InstagramEmbedCodeConstraint;
 use Drupal\media_entity_instagram\Plugin\Validation\Constraint\InstagramEmbedCodeConstraintValidator;
 use Drupal\Tests\UnitTestCase;
@@ -50,17 +49,12 @@ class ConstraintsTest extends UnitTestCase {
     $validator = new InstagramEmbedCodeConstraintValidator();
     $validator->initialize($execution_context);
 
-    // We need to mock the string_long field definition so that the validator
-    // will call StringLongItem::mainPropertyName() in getValue().
-    $definition = $this->prophesize(FieldDefinitionInterface::class);
-    $definition->getClass()->willReturn(StringLongItem::class);
+    $definition = $this->getMock(ComplexDataDefinitionInterface::class);
+    $definition->method('getPropertyDefinitions')->willReturn([]);
 
-    // $data is a mock string_long field item which contains the embed code.
-    $data = $this->prophesize(FieldItemInterface::class);
-    $data->getFieldDefinition()->willReturn($definition->reveal());
-    $data->get('value')->willReturn($embed_code);
-
-    $validator->validate($data->reveal(), $constraint);
+    $data = new StringLongItem($definition);
+    $data->set('value', $embed_code);
+    $validator->validate($data, $constraint);
   }
 
   /**
